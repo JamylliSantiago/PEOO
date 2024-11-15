6fa36d3322a32500e7115de76a728c19c9b4088a
JamylliSantiago <Jamylli.santiago@escolar.ifrn.edu.br>
2024-11-15 15:38:05 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,8 +1,1 @@
-#Faça um programa que calcula a média de três notas fornecidas pelo usuário.
 
-a = float(input("Primeiro número:"))
-b = float(input("Segundo número:"))
-c = float(input("Terceiro número:"))
-
-Média = (a + b + c) / 3
-print(Média)
